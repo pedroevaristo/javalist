737b9fff8593fcf34cfb75edc15aa25ea969342b
pedroevaristo <hattoriminamoto@gmail.com>
2022-04-12 21:48:15 -0300

    120422

--- src/javaapplication3/Pratica5.java
+++ src/javaapplication3/Pratica5.java
@@ -1,9 +1,27 @@
 
 package javaapplication3;
-
+//import java.util.Scanner;
 public class Pratica5 {
 
     public static void main(String args[]) {
+        int[] vetor = new int[4];
+        int[] resul = new int[4];
+java.util.Scanner scan = new java.util.Scanner(System.in);
+        int loop=0;
+        while(loop<4){
+            vetor[loop] = scan.nextInt();
+            resul[loop]= (int) Math.pow(vetor[loop],2);
+            loop+=1;
+        }
+        
+        
+        for(int t =0; t<4;t++){
+            if(resul[t] > 100){
+                System.out.println("Maior que 100 = "+resul[t]);
+            }
+            System.out.println(resul[t]);
+        }
+        
         
     }
 }
--- src/javaapplication3/Pratica6.java
+++ src/javaapplication3/Pratica6.java
@@ -1,19 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-package javaapplication3;
-
-/**
- *
- * @author pedro
- */
-public class Pratica6 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+import package javaapplication3;
+import java.util.Scanner;
+public class Pratica6{
+  public static void main(String[] args){
+    int acumulador =0;
+    Scanner scan = new Scanner(System.in);
+    int[] array = new int[3];
+  int l = 0;//loop
+    while(l < 3){
+     array[l]= scan.nextInt();
+      acumulador+= array[l];
+      l+=1;
     }
+    int total = acumulador / 3;
+    if(acumulador / 3 == 6){
+      System.out.println("Ta de recuperação");
+    }
+    int total = acumulador / 3;
+    if(total > 6){
+System.out.println("Ta aprovado");
+      
+    }
+    if(total < 6){
+      System.out.println("Ta reprovado");
+    }
+    
+  
+  
 }
